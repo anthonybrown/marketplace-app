fa52d10c76e2c38a75b9bf27870fbe997d8b4569
Tony Brown <tony@tonybrown.me>
2022-06-07 11:54:21 -0400

    able to sign up users

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,11 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth"
+import { db } from "../firebase.config"
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg"
 import visibilityIcon from "../assets/svg/visibilityIcon.svg"
 
@@ -22,6 +28,30 @@ function SignUp() {
     }))
   }
 
+  const handleOnSubmit = async e => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCrendential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      )
+
+      const user = userCrendential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      })
+
+      navigate("/")
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <div className='pageContainer'>
@@ -30,7 +60,7 @@ function SignUp() {
         </header>
 
         <main>
-          <form>
+          <form onSubmit={handleOnSubmit}>
             <input
               className='nameInput'
               type='text'
