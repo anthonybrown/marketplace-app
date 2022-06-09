e5f00d649a99865f69eaaf4d3d8caf54601e3a43
Tony Brown <tony@tonybrown.me>
2022-06-09 16:05:49 -0400

    create map with leaflet

---
+++ src/pages/Contact.js
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react"
+import { useParams, useSearchParams } from "react-router-dom"
+import { doc, getDoc } from "firebase/firestore"
+import { db } from "../firebase.config"
+import { toast } from "react-toastify"
+import { async } from "@firebase/util"
+
+function Contact() {
+  const [message, setMessage] = useState("")
+  const [landlord, setLandlord] = useState(null)
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const params = useParams()
+
+  useEffect(() => {
+    const getLandlord = async () => {
+      const docRef = doc(db, "users", params.landlordId)
+      const docSnap = await getDoc(docRef)
+
+      if (docSnap.exists()) {
+        setLandlord(docSnap.data())
+      } else {
+        toast.error("Could not get landlord data")
+      }
+    }
+
+    getLandlord()
+  }, [params.landlordId])
+
+  const handleChange = e => {
+    setMessage(e.target.value)
+  }
+
+  return (
+    <div className='pageContainer'>
+      <header>
+        <p className='pageHeader'>Contact Landlord</p>
+      </header>
+      {landlord !== null && (
+        <main>
+          <div className='contactLandlord'>
+            <p className='landlordName'>
+              Contact {landlord?.name}
+            </p>
+          </div>
+          <form className='messageForm'>
+            <div className='messageDiv'>
+              <label htmlFor='message' className='messageLabel'>
+                Message
+              </label>
+              <textarea
+                className='textarea'
+                name='message'
+                id='message'
+                value={message}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+
+            <a
+              href={`mailto:${
+                landlord.email
+              }?Subject=${searchParams.get(
+                "listingName",
+              )}&body=${message}`}
+            >
+              <button className='primaryButton' type='button'>
+                Send Message
+              </button>
+            </a>
+          </form>
+        </main>
+      )}
+    </div>
+  )
+}
+
+export default Contact
--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -1,4 +1,10 @@
 import { useEffect, useState } from "react"
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+} from "react-leaflet"
 import { Link, useNavigate, useParams } from "react-router-dom"
 import { getDoc, doc } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
@@ -90,7 +96,32 @@ function Listing() {
           <li>{listing.parking && "Parking Spot"}</li>
           <li>{listing.furnished && "Furnished"}</li>
           <p className='listingLocationTitle'>Location</p>
-          {/* MAP */}
+
+          <div className='leafletContainer'>
+            <MapContainer
+              style={{ height: "100%", width: "100%" }}
+              center={[
+                listing.geolocation.lat,
+                listing.geolocation.lng,
+              ]}
+              zoom={13}
+              scrollWheelZoom={false}
+            >
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+              />
+
+              <Marker
+                position={[
+                  listing.geolocation.lat,
+                  listing.geolocation.lng,
+                ]}
+              >
+                <Popup>{listing.location}</Popup>
+              </Marker>
+            </MapContainer>
+          </div>
 
           {auth.currentUser?.uid !== listing.userRef && (
             <Link
