02a071cc46e8a01fdbabb1155d1a8d97a651a490
Tony Brown <tony@tonybrown.me>
2022-06-10 09:14:12 -0400

    add config

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0FolCUS46nMv5sIqG_IVS01xiXMM2ygg",
+  authDomain: "marketplace-75d5e.firebaseapp.com",
+  projectId: "marketplace-75d5e",
+  storageBucket: "marketplace-75d5e.appspot.com",
+  messagingSenderId: "789176675742",
+  appId: "1:789176675742:web:e9c3e1826fa8df0580fa8f",
+  measurementId: "G-CLQHFFQGKD",
+}
+
+export const db = getFirestore()
